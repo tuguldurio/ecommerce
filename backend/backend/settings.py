06257e704c657b008587aabb4075750899149b1d
tuguldurio <ok.tuguldur@gmail.com>
2021-05-11 13:29:01 -0400

    .

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ecommerce',
         'USER': 'postgres',
-        'PASSWORD': 'password',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     },
